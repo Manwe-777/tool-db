8e9013d6e7b3cd8be2d48a5bd07bdc2d2640a256
Manwë <manuel.etchegaray7@gmail.com>
2025-12-29 01:47:46 -0300

    Update index.ts

--- packages/redis-store/lib/index.ts
+++ packages/redis-store/lib/index.ts
@@ -3,28 +3,23 @@ import { createClient } from "redis";
 
 export default class toolDbRedis extends ToolDbStorageAdapter {
   private redisClient = createClient();
-  private readyPromise: Promise<void>;
 
   constructor(db: ToolDb, forceStorageName?: string) {
     super(db, forceStorageName);
 
-    this.readyPromise = this.redisClient.connect().then(() => {});
-  }
-
-  private async waitForReady() {
-    await this.readyPromise;
+    this._readyPromise = this.redisClient.connect().then(() => { });
   }
 
   public async put(key: string, data: string) {
     await this.waitForReady();
-    
+
     await this.redisClient.set(key, data);
     return data;
   }
 
   public async get(key: string) {
     await this.waitForReady();
-    
+
     // console.warn("store get", key);
     const v = await this.redisClient.get(key);
     if (v) return v;
@@ -33,7 +28,7 @@ export default class toolDbRedis extends ToolDbStorageAdapter {
 
   public async query(key: string) {
     await this.waitForReady();
-    
+
     const v = await this.redisClient.keys(key + "*");
     if (v) return v;
     else throw new Error("Error retrieving data");
